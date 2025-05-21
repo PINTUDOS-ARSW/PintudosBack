db476378d70fca1972ddc83fe44b730dba4ff3c7
Diego <58009401+marzo245@users.noreply.github.com>
2025-05-21 12:15:03 -0500

    fix bugs

--- src/main/java/pintudos/game/controller/TraceDTO.java
+++ src/main/java/pintudos/game/controller/TraceDTO.java
@@ -2,9 +2,13 @@ package pintudos.game.controller;
 
 import java.util.List;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import pintudos.game.model.CustomPoint;
 
 @Data
+@Getter
+@Setter
 public class TraceDTO {
 
   private String roomId;
